36e471951095361855559dbbc02fd001845513a8
Subhendu Sekhar Baug <subhendu.baug@gmail.com>
2020-08-05 22:59:21 +0530

    insertion

---
+++ insertion_sort.py
@@ -0,0 +1,13 @@
+def insertion_sort(array):
+    for i in range(len(array)-1):
+        key_item = array[i]
+        j = i - 1
+        while j >= 0 and array[j] > key_item:
+            array[j + 1] = array[j]
+            j -= 1
+        array[j + 1] = key_item
+    return array
+
+if __name__ == "__main__":
+    arr = [9,6,89,24,67]
+    print(insertion_sort(arr))
